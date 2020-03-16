1710008d2e44ebf86dcb0a7ed43d6f031511b22d
Nash <nashsmith0510@gmail.com>
2020-03-16 13:51:00 +1300

    Add Constructors.

--- LZWCompression/src/Trie.java
+++ LZWCompression/src/Trie.java
@@ -2,10 +2,26 @@ class Trie {
 
   TrieNode _root;
 
+  public Trie(){
+    //set root
+    _root = new TrieNode(0);
+    Byte b = new Byte(0);
+    //prime the roots search tree with all byte patterns
+    for(int i = 0; i < 256; i++){
+      _root._values.insert(b);
+      b++;
+    }
+  }
+
   class TrieNode {
 
     protected int _phraseKey;
     protected SearchTree _values;
+
+    public TrieNode(int phraseKey){
+      _phraseKey = phraseKey;
+      _values = new SearchTree();
+    }
 
     public int getPhraseKey(){
       return _phraseKey;
