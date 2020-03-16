9d56f78ff60265a7b180a2d269e79f879cd3d5c8
Nash <nashsmith0510@gmail.com>
2020-03-16 13:01:24 +1300

    Pseudo code

--- LZWCompression/src/LZWencode.java
+++ LZWCompression/src/LZWencode.java
@@ -1,4 +1,15 @@
-
 public class LZWencode {
 
+  public static void main (String[] args){
+    //get input
+    //find first inputs phrase number
+    //check if the next input is in the Trie
+    //YES
+      //find the phrase number of the inputs
+      //check if the next input is in the Trie ^
+    //NO
+      //add the next input into the Trie
+      //update the bits needed to encode phrase number (Ceiling of Log2 P or 2^? = P)
+      //output the phrase number of the first input
+  }
 }
