e30203c99810dd3466c9925f9cf48d34fc55da80
cornpoid <cornpoid@users.noreply.github.com>
2020-03-22 17:05:48 +1300

    Pack pseudocode and some commenting on Phrase class

--- LZWCompression/src/LZWpack.java
+++ LZWCompression/src/LZWpack.java
@@ -1,4 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class LZWpack {
-
+	public static void main (String[] args) throws IOException{
+		//take in input file
+		File file = new File(args[0]);
+		Scanner input = new Scanner(file);
+		//find largest bit required by iterating through file
+		//output largest bit required
+		//initialise byte buffer
+		//initialise buffer space b = 8
+		//while file is not empty
+			//get next 32-bit integer
+			//set x to largest bit required
+			//while x is not 0
+				//if x > 8
+					//shift right x - b where x = number of significant bits left and b is spaces left on buffer
+					//set x to x - b
+					//cast to byte through OR operation
+					//output byte
+				//else
+					//shift left 8 - x
+					//cast to byte through OR operation
+					//set b to b - x
+					//set x to 0
+		//output byte 
+	}
 }
--- LZWCompression/src/Phrase.java
+++ LZWCompression/src/Phrase.java
@@ -9,13 +9,14 @@ public class Phrase {
 		pattern = b;
 	}
 	
-	
+	//calls the return pattern method and returns the stack of patterns
 	public Stack<Byte> getPattern(Map<Integer, Phrase> map) {
 		Stack<Byte> s = new Stack<Byte>();
 		returnPattern(map, s);
 		return s;
 	}
 	
+	//iterates through the phrase in the map creating a stack with the last pattern on top
 	private void returnPattern(Map<Integer, Phrase> map, Stack<Byte> stack){
 		if(phrasenumber == null) {
 			stack.push(pattern);
