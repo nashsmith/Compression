f43adcde9fd775d364dd0a458116dc808f19b058
Nash <nashsmith0510@gmail.com>
2020-03-31 13:22:34 +1300

    Testing program

--- LZWCompression/src/LZWencode.java
+++ LZWCompression/src/LZWencode.java
@@ -6,17 +6,26 @@ public class LZWencode {
 
   public static void main (String[] args) throws IOException{
     //get input
-	byte[] fileContent = Files.readAllBytes(Paths.get(args[0]));
+    byte[] temp = Files.readAllBytes(Paths.get(args[0]));
+    Byte[] fileContent = new Byte[temp.length];
+    for(int i = 0; i < temp.length; i++){
+      fileContent[i] = new Byte(Byte.valueOf(temp[i]));
+    }
+    //setup Trie phrase dictionary
+    Trie phraseList = new Trie(true);
+
     //find first inputs phrase number
+    Byte[] firstInput = {new Byte(fileContent[0])};
+    int phrasekey = phraseList.findPhrase(firstInput);
+    System.out.print(Integer.toString(phrasekey));
+
     //check if the next input is in the Trie
     //YES
-      //find the phrase number of the inputs
-      //check if the next input is in the Trie ^
+    //find the phrase number of the inputs
+    //check if the next input is in the Trie ^
     //NO
-      //add the next input into the Trie
-      //update the bits needed to encode phrase number (Ceiling of Log2 P or 2^? = P)
-      //output the phrase number of the first input
+    //add the next input into the Trie
+    //update the bits needed to encode phrase number (Ceiling of Log2 P or 2^? = P)
+    //output the phrase number of the first input
   }
 }
-
-	
